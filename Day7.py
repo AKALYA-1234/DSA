6f8e0af82e542ece7a707c4e310bc903bb372674
Akalya <akalyas.ad24@bitsathy.ac.in>
2025-08-31 22:01:31 +0530

    Updated code - added Day6 programs

--- Day7.py
+++ Day7.py
@@ -1,6 +1,5 @@
 #Check Palindrome & GCD Or HCF
 import math
-
 def is_palindrome(n):
     temp = n
     rev = 0
@@ -13,7 +12,7 @@ def is_palindrome(n):
 def find_gcd(a, b):
     return math.gcd(a, b)
 
-choice = int(input("Enter 1 for Palindrome Check, 2 for GCD/HCF: "))
+choice = int(input("Enter 1 for Palindrome Check,2 for GCD/HCF: "))
 
 if choice == 1:
     num = int(input("Enter a number:"))
@@ -25,7 +24,7 @@ if choice == 1:
 elif choice == 2:
     a = int(input("Enter first number: "))
     b = int(input("Enter second number: "))
-    print("GCD / HCF of", a, "and", b, "is:", find_gcd(a, b))
+    print("GCD / HCF of", a, "and", b, "is:",find_gcd(a, b))
 
 else:
     print("Invalid choice!")
